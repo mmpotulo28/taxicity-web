84cd53380ff20cc36f4ba0d843753e2b313e1e89
mmpotulo28 <mpotulom28@gmail.com>
2026-02-12 16:13:14 +0200

    feat(dependencies): update turbo version to 2.8.7 and add @swc/core as a dev dependency

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -2,13 +2,10 @@ import pino from "pino";
 
 export const logger = pino({
 	level: process.env.NODE_ENV === "production" ? "info" : "debug",
-	transport:
-		process.env.NODE_ENV !== "production"
-			? {
-					target: "pino-pretty",
-					options: {
-						colorize: true,
-					},
-				}
-			: undefined,
+	transport: {
+		target: "pino-pretty",
+		options: {
+			colorize: true,
+		},
+	},
 });
